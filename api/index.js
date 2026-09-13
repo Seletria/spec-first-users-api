c45578c35bcae7b92aa2d0596176a16ecfe7cf37
Seletria <esra.kocaoglu.contact@gmail.com>
2026-09-13 13:53:45 +0300

    feat: load environment variables via dotenv in api entrypoint

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const usersRoutes = require('./routes/users.routes');
 const healthCheck = require('./routes/healthCheck.routes');
